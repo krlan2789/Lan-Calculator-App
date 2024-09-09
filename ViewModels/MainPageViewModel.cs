654fc3f9318f1f355a4f378575e89bba6a3f55d0
Erlan Kurnia <33871129+krlan2789@users.noreply.github.com>
2024-09-09 17:31:27 +0700

    Fix command's naming . .

--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Lan_Calculator_App.ViewModels
 {
@@ -26,8 +25,95 @@ namespace Lan_Calculator_App.ViewModels
         [RelayCommand]
         private void OnButtonClicked(string text)
         {
-            Debug.WriteLine(text);
-            ExpressionDisplay += text;
+            if (text == "AC")
+            {
+                ExpressionDisplay = "";
+            }
+            else
+            {
+                if (text.Replace(" ", "") == "()")
+                {
+                    char lastChar = ExpressionDisplay.Length > 0 ? ExpressionDisplay[^1] : ' ';
+                    if (string.IsNullOrEmpty(ExpressionDisplay) || IsOperator(lastChar))
+                        text = "(";
+                    else
+                        text = ")";
+                }
+
+                ExpressionDisplay += text;
+                Debug.WriteLine(ExpressionDisplay);
+
+                //while (true)
+                //{
+                //    string priorityFormula = GetPriorityFormula(text);
+                //    Debug.WriteLine($"{priorityFormula}");
+                //    //text = text.Replace($"({priorityFormula})", "$$");
+                //    break;
+                //}
+            }
+        }
+
+        private long Calculate(string value)
+        {
+            long result = 0;
+
+            switch (value)
+            {
+                case "%":
+                    break;
+                case "*":
+                    break;
+                case "/":
+                    break;
+                case "-":
+                    break;
+                case "+":
+                    break;
+                case "=":
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
+        }
+        
+        private static bool IsOperator(char value)
+        {
+            return value switch
+            {
+                '%' or '*' or '/' or '-' or '+' or '=' or '(' or ')' => true,
+                _ => false,
+            };
+        }
+
+        private static string GetPriorityFormula(string value)
+        {
+            string selectedFormula = "";
+
+            int startBracketCount = value.Replace("(", "").Length;
+            int endBracketCount = value.Replace(")", "").Length;
+
+            if (startBracketCount == endBracketCount)
+            {
+                int openIndex = 0;
+                int closeIndex = value.IndexOf(')');
+
+                for (int i = value.IndexOf('('); i < closeIndex; i = value.IndexOf('(', i + 1))
+                {
+                    if (i < closeIndex)
+                    {
+                        openIndex = i;
+                    }
+                    else break;
+                }
+                int length = value.Length - openIndex - closeIndex;
+                if (length < 0) length = 1;
+                Debug.WriteLine($"{openIndex} -> {length}", "PriorityFormula");
+                if (value.Length > length) selectedFormula = value.Substring(openIndex, length);
+            }
+
+            return selectedFormula;
         }
     }
 }
